88d33c4e3ead90c9149c3dea833379e2008f029c
GBOXDEO <159724599+GBOXDEO@users.noreply.github.com>
2024-02-27 07:51:00 +0800

    Update itv_test.py

--- itv_test.py
+++ itv_test.py
@@ -7,7 +7,7 @@ import requests
 import re
 
 urls = [
-    "https://fofa.info/result?qbase64=aXA9IjIyMC4xNjMuMTc4LjE0NCI%3D",    #澳门
+    "https://fofa.info/result?qbase64=aGVhZGVyPSJBY2NlcHQtUmFuZ2VzOiBieXRlcyI%3D",    #澳门
     ]
 
 def modify_urls(url):
